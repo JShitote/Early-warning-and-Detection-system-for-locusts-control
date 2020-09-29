670498ff67d4df45e214b7dc24e741cd4a2cb8ac
skngetich <stephenngetich5@gmail.com>
2020-09-29 07:17:38 +0300

    functions init

--- cloud-app/functions/index.js
+++ cloud-app/functions/index.js
@@ -1,4 +1,8 @@
 const functions = require('firebase-functions');
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +11,22 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const config = functions.config()
+const app = express();
+
+
+app.use(cors({
+  origin: true
+}));
+app.use(bodyParser.json());
+
+app.get('/',(req,res)=>{
+  res.send({hello:"hello"})
+})
+// Testing express endpoints
+exports.app = app;
+
+// Firebase endpoint
+exports.api = functions.https.onRequest(app);
+
